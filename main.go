26c8dd98b1fb8bce28a9754bac52ef3621e934f5
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-08-21 09:13:40 +0300

    feat: add function StartEchoServer

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"net"
+)
+
+var (
+	ErrorPortEmpty = errors.New("port cannot be empty")
+)
+
+func StartEchoServer(port string) error {
+	if port == "" {
+		return ErrorPortEmpty
+	}
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		return fmt.Errorf("failed to create TCP listener: %w", err)
+	}
+	defer listener.Close()
+
+	fmt.Printf("TCP Echo Server listening on %s\n", port)
+	fmt.Println("Waiting for connection...")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		return fmt.Errorf("failed to accept connection: %w", err)
+	}
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		if _, err = conn.Write([]byte(scanner.Text() + "\n")); err != nil {
+			return fmt.Errorf("Write error to %s: %w\n", conn.RemoteAddr(), err)
+		}
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Happy coding!")
+	if err := StartEchoServer(":8080"); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
 }
