1e0cf2e4cac45dd2e4c280d0cd413ddcfe0d31a6
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-08-23 00:38:48 +0300

    feat:  add Client struct and multi-client support

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"net"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 var (
@@ -17,6 +19,12 @@ type ChatMessage struct {
 	ClientID    string
 	Content     string
 	MessageType string
+}
+
+type Client struct {
+	ID       string
+	Conn     net.Conn
+	JoinTime time.Time
 }
 
 func FormatMessage(msg ChatMessage) string {
@@ -37,6 +45,33 @@ func ParseIncomingMessage(raw, senderID string) ChatMessage {
 	}
 }
 
+func HandleClient(client *Client) error {
+	defer client.Conn.Close()
+
+	conn := client.Conn
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		content := scanner.Text()
+		msg := ParseIncomingMessage(content, client.ID)
+		formattedMsg := FormatMessage(msg)
+
+		if _, err := conn.Write([]byte(formattedMsg + "\n")); err != nil {
+			return fmt.Errorf("write error to %s: %w", conn.RemoteAddr(), err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("scanner error: %w", err)
+	}
+
+	return nil
+}
+
+func GenerateClientID() string {
+	return uuid.New().String()[:8]
+}
+
 func StartEchoServer(port string) error {
 	if port == "" {
 		return ErrorPortEmpty
@@ -48,26 +83,28 @@ func StartEchoServer(port string) error {
 	defer listener.Close()
 
 	fmt.Printf("TCP Echo Server listening on %s\n", port)
-	fmt.Println("Waiting for connection...")
 
-	conn, err := listener.Accept()
-	if err != nil {
-		return fmt.Errorf("failed to accept connection: %w", err)
-	}
-	defer conn.Close()
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		content := scanner.Text()
-		msg := ParseIncomingMessage(content, "User_001")
-		formatedMsg := FormatMessage(msg)
-
-		if _, err = conn.Write([]byte(formatedMsg + "\n")); err != nil {
-			return fmt.Errorf("write error to %s: %w", conn.RemoteAddr(), err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return fmt.Errorf("failed to accept connection: %w", err)
 		}
-	}
+		go func() {
+			client := &Client{
+				ID:       "User_" + GenerateClientID(),
+				Conn:     conn,
+				JoinTime: time.Now(),
+			}
 
-	return nil
+			fmt.Printf("Client %s connected\n", client.ID)
+
+			if err = HandleClient(client); err != nil {
+				fmt.Printf("Client %s error: %v\n", client.ID, err)
+			}
+
+			fmt.Printf("Client %s disconnected\n", client.ID)
+		}()
+	}
 }
 
 func main() {
